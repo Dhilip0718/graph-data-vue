872d859e6d963dd488bf0192ccf7d0e62182a3ff
Dhilip0718 <dhilipram@gmail.com>
2024-04-23 22:34:57 +0200

    initial code setup for node api

---
+++ server.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = 3000;
+
+// Sample graph data to be edited later with DB data
+const graphData = {
+    "data": [
+        {
+            "name": "A",
+            "description": "This is a description of A",
+            "parent": ""
+        },
+        {
+            "name": "B",
+            "description": "This is a description of B",
+            "parent": "A"
+        },
+        {
+            "name": "C",
+            "description": "This is a description of C",
+            "parent": "A"
+        },
+        {
+            "name": "D",
+            "description": "This is a description of D",
+            "parent": "A"
+        },
+        {
+            "name": "B-1",
+            "description": "This is a description of B-1",
+            "parent": "B"
+        },
+        {
+            "name": "B-2",
+            "description": "This is a description of B-2",
+            "parent": "B"
+        },
+        {
+            "name": "B-3",
+            "description": "This is a description of B-3",
+            "parent": "B"
+        }
+    ]
+}
+
+app.use(bodyParser.json());
+
+// Endpoint to serve graph data
+app.get('/graph-data', (req, res) => {
+    res.json(graphData);
+});
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
